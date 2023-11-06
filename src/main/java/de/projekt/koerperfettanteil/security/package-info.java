3079f7df34c470ae24efed37a1664f6eaea4c2b4
Sabi <g.szabsy@azosteam.de>
2023-11-06 17:13:34 +0100

    Initial version of koerperfettanteil generated by generator-jhipster@7.9.3

---
+++ src/main/java/de/projekt/koerperfettanteil/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package de.projekt.koerperfettanteil.security;
---
+++ src/main/java/de/projekt/koerperfettanteil/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package de.projekt.koerperfettanteil.web.rest;
